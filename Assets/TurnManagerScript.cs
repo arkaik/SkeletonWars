c20ea052eab3f66c4e65e6b11f1cfd5a6bf3b925
falc@null.net <falc@null.net>
2016-10-29 01:20:36 +0200

    TurnManager basics

--- Assets/TurnManagerScript.cs
+++ Assets/TurnManagerScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TurnManagerScript : MonoBehaviour {
 
@@ -7,21 +8,38 @@ public class TurnManagerScript : MonoBehaviour {
 	public int playerNum = 2;
 	public int iaNum = 0;
 	public bool inTurn = true;
+	public List<List<GameObject> > charList;
+
+	private int actualPlayer;
+	private List<GameObject> actualTeam;
+	private int actualChar;
 
 	// Use this for initialization
 	void Start () {
-		
+		//Estamos en turno
+		inTurn = true;
+		actualPlayer = 0;
+		actualTeam = charList [actualPlayer];
+		actualChar = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		while (inTurn) {
-		
+
+		if (actualTeam [actualChar].hasActioned ()) {
+			actualChar += 1;
+		}
+			
+		if (actualChar == actualTeam.Count) {
+			changeTeam ((actualPlayer + 1) % playerNum);
 		}
 	}
 
-	void Turn (int team) {
-		
+	void changeTeam (int newTeam) {
+		actualPlayer = newTeam;
+		actualTeam = charList [actualPlayer];
+		actualChar = 0;
 	}
+
 }
 
