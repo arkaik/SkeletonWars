096081b12fc788abb25cfe2e14e6ccf606a37a79
falc@null.net <falc@null.net>
2016-10-29 00:25:10 +0200

    lolmao

--- Assets/TurnManagerScript.cs
+++ Assets/TurnManagerScript.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class TurnManagerScript : MonoBehaviour {
 
+	public int turnNum = 0;
+	public int playerNum = 2;
+	public int iaNum = 0;
+	public bool inTurn = true;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		while (inTurn) {
+		
+		}
+	}
+
+	void Turn (int team) {
+		
 	}
 }
+
