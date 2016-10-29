074f11f3f45b51d2b69314fffd3395dfcfc4d5cc
Llistoman <josepll.plamarcell@gmail.com>
2016-10-29 02:32:41 +0200

    generator script to instantiate everything

---
+++ Assets/Generator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+
+	GameObject tmanager;
+	GameObject fcreator;
+	GameObject ucreator;
+	Component tScript;
+	Component fScript;
+	Component uScript;
+
+	// Use this for initialization
+	void Start () {
+		GameObject turnMan = Instantiate (tmanager);
+		GameObject floorCreator = Instantiate (fcreator);
+		GameObject unitCreator = Instantiate (ucreator);
+		tScript = turnMan.GetComponent ("TurnManagerScript");
+		//fScript = floorCreator.GetComponent ("FloorCreatorScript");
+		uScript = unitCreator.GetComponent ("UnitCreator");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
